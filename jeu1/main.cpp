0cb53623be5d9548ff11cbfaf1812d3b794c1bc0
Tom Planche <58936594+TomPlanche@users.noreply.github.com>
2021-09-10 10:24:55 +0200

    continue de commenter

--- jeu1/main.cpp
+++ jeu1/main.cpp
@@ -1,7 +1,7 @@
 /**
   * @file main.cpp
   * @author Tom PLANCHE / Mathis HÉRIVEAU
-  * @brief Jeu du Devine Nombre
+  * @brief Jeu du Devine Nombre (pack 13) - S1.01 TD2 TP4
   * @date 08-09-2021
  **/
 
@@ -16,8 +16,17 @@ using namespace std;
 
 int randomTrouveInternet(int borneBasse, int borneHaute)
 {
-    random_device rd;  // Will be used to obtain a seed for the random number engine
-    mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
+    /**
+      * Programme : randomTrouveInternet
+      * But : Générer un nombre aléatoire entre deux bornes entières. Trouvé sur internet car le random de Patrick Etcheverry ne fonctionnait pas
+      * @author Bill Lynch
+      * Date de dernière modification :  08-09-21
+      * Remarques : les deux nombres doivent être des entiers
+      * @param borneBasse entier définissant la borne basse de génération
+      * @param borneHaute entier définissant la borne haute de génération
+      **/
+    random_device rd; 
+    mt19937 gen(rd());
     uniform_int_distribution<> distrib(borneBasse, borneHaute);
 
     return int(distrib(gen));
@@ -26,6 +35,15 @@ int randomTrouveInternet(int borneBasse, int borneHaute)
 
 int insererNombre(const char* phrase)
 {
+    /**
+     *  Programme : insererNombre
+     *  But :  Demandez, avec une phrase spéciale, à l'utilisateur de saisir un nombre
+     *  Date de dernière modification : 10 septembre 2021
+     *  Auteur : T. Planche / M. Hériveau
+     *  Remarques : L'utilisateur doit saisir un entier
+     *  @param nombre entier demandé à l'utilisateur
+     *  @param phrase phrase à afficher lors de la saisie du nombre
+    **/
     int nombre;
 
     while (true)
@@ -33,11 +51,11 @@ int insererNombre(const char* phrase)
         cout << phrase;
         cin >> nombre;
 
-        if (cin.fail()) // no extraction took place
+        if (cin.fail()) // si aucune extraction n'a eu lieu
         {
-            cin.clear(); // reset the state bits back to goodbit so we can use ignore()
-            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // clear out the bad input from the stream
-            continue; // try again
+            cin.clear(); // réinitialise les bits d'état à goodbit pour pouvoir utiliser ignore()
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // efface les mauvaises entrées 
+            continue;
         }
 
         break;
@@ -54,7 +72,7 @@ int main(void)
      *  But :  L'utilisateur doit trouver un nombre à partir des aides énoncées
      *  Date de dernière modification : 8 septembre 2021
      *  Auteur : T. Planche / M. Hériveau
-     *  Remarques : 
+     *  Remarques : Le programme commence le jeu
      *  @param borneBasse entier demandé à l'utilisateur définissant la borne basse de la recherche
      *  @param borneHaute entier demandé à l'utilisateur définissant la borne haute de la recherche
      *  @param choixTentatives entier demandé à l'utilisateur définissant le nombre de tentatives qu'il souhaite avoir
@@ -86,21 +104,22 @@ int main(void)
     // Idem pour la borne haute.
     borneHaute = insererNombre("Saisissez la borne haute : ");
 
-    // Si la borne basse est plus grande que la borne haute
+    // Si la borne basse est plus grande que la borne haute ou que les deux bornes ont la même valeur
     while (borneBasse == borneHaute || borneBasse > borneHaute) {
 
         cout << "Veuillez entrer des valeurs valides (bornes différentes ou borne basse < borne haute)" << endl;
 
         borneBasse = insererNombre("Saisissez la borne basse : ");
-
-        borneBasse = insererNombre("Saisissez la borne basse : ");
+        borneHaute = insererNombre("Saisissez la borne haute : ");
     }
 
+    // Génère un nombre aléatoire entre les deux bornes choisies
     nb_a_deviner = randomTrouveInternet(borneBasse, borneHaute);
 
-
+    // La valeur tentative prends la valeur de la borne basse moins un, cela permet de pouvoir utiliser la boucle ligne 132
     tentative = borneBasse - 1;
 
+    // Demande à l'utilisateur le nombre de tentatives qu'il souhaite avoir
     choixTentatives = insererNombre("\nCombien de vies voulez vous avoir (minimum 1) ? : ");
 
     // Ici, je convertis la phrase avec les variables des bornes en char, afin de le passer en argument dans ma fonction "insererNombre".
@@ -109,10 +128,9 @@ int main(void)
     phraseChar = phraseString.c_str();
 
 
-
-
+    // Boucle qui tourne tant que la tentative de l'utilisateur n'est pas bonne ou qu'il a des tenetatives restantes
     while (tentative != nb_a_deviner)
-    {
+    {   
         nbDeTentatives++;
 
         tentative = insererNombre(phraseChar);
