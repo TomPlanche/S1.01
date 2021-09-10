9f9a686521fa027a80012c380dfd8536ab8bcccf
Tom Planche <58936594+TomPlanche@users.noreply.github.com>
2021-09-10 14:59:28 +0200

    creation du dosser et fichier pour le simon

---
+++ jeu du simon/main.cpp
@@ -0,0 +1,19 @@
+/**
+  * @file main.cpp
+  * @author T.Planche and M.Heriveau
+  * @brief fichier du jeu du simon
+  * @date 10 septembre 2021
+  **/
+
+int main(void) {
+    /*
+        Programme : main
+        But :  Lance le jeu
+        Date de dernière modification : 10 septembre 2021
+        Auteur : T. Planche
+        Remarques : pas commencé
+    */
+
+    // INITIALISATION
+    return 0;
+}
