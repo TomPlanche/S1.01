da6a18c13abfbcd615b8c1f97cc57321130a9033
Tom Planche <58936594+TomPlanche@users.noreply.github.com>
2021-09-12 15:14:21 +0200

    main cpp commencé

--- jeu du simon/main.cpp
+++ jeu du simon/main.cpp
@@ -5,6 +5,38 @@
   * @date 10 septembre 2021
   **/
 
+#include <iostream>
+#include <random>
+#include <unistd.h>
+
+using namespace std;
+
+
+
+
+
+int randomTrouveInternet(int borneBasse, int borneHaute)
+{
+    /**
+      * Programme : randomTrouveInternet
+      * But : Générer un nombre aléatoire entre deux bornes entières. Trouvé sur internet car le random de Patrick Etcheverry ne fonctionne pas
+      * @author Bill Lynch
+      * Date de dernière modification :  08-09-21
+      * Remarques : les deux nombres doivent être des entiers
+      * @param borneBasse entier définissant la borne basse de génération
+      * @param borneHaute entier définissant la borne haute de génération
+      **/
+
+    random_device rd; 
+    mt19937 gen(rd());
+    uniform_int_distribution<> distrib(borneBasse, borneHaute);
+
+    if ((int(distrib(gen))) == 0) { // La variable tentative étant initiée à 0, cela évite que le programme dise que le joueur à gagné sans même jouer.
+        return randomTrouveInternet(borneBasse, borneHaute);
+    }
+    return int(distrib(gen));
+}
+  
 int main(void) {
     /*
         Programme : main
@@ -14,8 +46,35 @@ int main(void) {
         Remarques : pas commencé
     */
     char choixUtilisateur;
+    string choixDisponibles = "pfc";
 
     // TODO : Commencer l'algo
+    // ! : Trouver fonction sleep.
+
+
+    cout << choixDisponibles[2] << endl;
+    cout << choixDisponibles.find('p') << endl;
+
+    cin >> choixUtilisateur;
+
+    cout << (string("pfc").find(choixUtilisateur) != 0) << endl;
+
+    system("clear");
+
+
+
+    cout << "ça marche ?! ";
+
+    sleep(2);
+
+    cout << "oui " << endl;
+
+    
+
+
+    cout << "YESSSSS";
+
+
 
     // INITIALISATION
     return 0;
