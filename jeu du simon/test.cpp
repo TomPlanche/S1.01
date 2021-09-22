bfc609b8bdc0f74378a16467d17d5edc77d10c6a
Tom Planche <58936594+TomPlanche@users.noreply.github.com>
2021-09-22 15:00:13 +0200

    j'y arrive pas aled

--- jeu du simon/test.cpp
+++ jeu du simon/test.cpp
@@ -3,8 +3,6 @@
 #include <cstdlib>
 #include <sstream>
 
-
-
 #ifdef __APPLE__
     #include <unistd.h>
     #include <stdlib.h>
@@ -13,6 +11,11 @@
 #ifdef _WIN32
     //#include "game-tools.h"
 #endif
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
 
 namespace OS
 {
@@ -23,6 +26,8 @@ namespace OS
         #elif __APPLE__
             system("clear");
         #endif
+
+        cout << "** Jeu du Simon **" << endl;
     }
 
     void sleep(int secondes)
@@ -66,13 +71,60 @@ void afficherTexteCouleur(const char* phraseEntree, char couleur, bool retourALa
         phraseString += "\n";
     }
 
-    std::cout << phraseString;
+    cout << phraseString;
+}
+
+
+int randomTrouveInternet(int borneBasse, int borneHaute)
+{
+    /**
+      * Programme : randomTrouveInternet
+      * But : Générer un nombre aléatoire entre deux bornes entières. Trouvé sur internet car le random de Patrick Etcheverry ne fonctionne pas
+      * @author Bill Lynch
+      * Date de dernière modification :  08-09-21
+      * Remarques : les deux nombres doivent être des entiers
+      * @param borneBasse entier définissant la borne basse de génération
+      * @param borneHaute entier définissant la borne haute de génération
+      **/
+
+    std::random_device rd; 
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(borneBasse, borneHaute);
+
+    if ((int(distrib(gen))) == 0) { // La variable tentative étant initiée à 0, cela évite que le programme dise que le joueur à gagné sans même jouer.
+        return randomTrouveInternet(borneBasse, borneHaute);
+    }
+    return int(distrib(gen));
+}
+
+
+vector<char> couleurRandom(vector<char> listeCouleurs) {
+    char listeToutesCouleurs[4] = {'v', 'r', 'b', 'j'};
+    int numeroAleatoire = randomTrouveInternet(1, 4);
+    listeCouleurs.push_back(listeCouleurs[numeroAleatoire]);
+    
+    return listeCouleurs;
 }
 
 
 int main() {
-    afficherTexteCouleur("test en rouge", 'r', false);
-    afficherTexteCouleur("test en bleu attaché au rouge", 'b', true);
-    afficherTexteCouleur("test en vert avec saut de ligne", 'v', false);
+    bool gagne;
+    const char* regles = "[v -> vert, r -> rouge, b -> bleu, j -> jaune]";
+    char couleursGeneres;
+    int nouvelleCouleurRandom;
+    vector <char> listeCouleursDefinies;
+
+    // OS::clear();
+
+    // while (! gagne)
+    // {
+    //     OS::clear();
+    //     nouvelleCouleurRandom = randomTrouveInternet(1, 3);
+    // }
+
+    listeCouleursDefinies = couleurRandom(listeCouleursDefinies);
+
+
+    
     return 0;
 }
